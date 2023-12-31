273bc4dda782d886adabba06c2234c8ef59e335d
Ste Devine <ste@stedevine.com>
2023-12-31 19:27:31 +0000

    2024 update

--- controllers/photo.js
+++ controllers/photo.js
@@ -1,12 +1,13 @@
 'use strict';
 
 function photoController($scope) {
+  console.log("Photo Controller")
   var photos = [];
   for (var i=1; i<8; i++){
     photos.push("./images/" + i+ ".jpg");
   }
-  var photoIndex = 0;
-  $scope.photoImg = photos[1];
+  var photoIndex = 1;
+  $scope.photoImg = photos[photoIndex];
 
   $scope.setPhoto = function() {
 
